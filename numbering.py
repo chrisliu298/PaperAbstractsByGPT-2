9b5cb16c121f2d0803ff176c489d8fbbd36a163d
yuhaoliu298 <yuhaoliu298@gmail.com>
2020-05-25 21:05:05 -0700

    Add numbering

--- numbering.py
+++ numbering.py
@@ -4,7 +4,10 @@ f = open("valid_set_generation.md", "r").readlines()
 with open("valid_set_generation_numbered.md", "w+") as numbering:
     count = 1
     for i in f:
-        numbering.write(f"{count}. " + i)
+        if count % 3 == 1:
+            numbering.write(f"{count}. " + i)
+        else:
+            numbering.write(i)
         count += 1
 
 numbering.close()
